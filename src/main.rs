dadda9b67e4a7768201c5ea1df05dffbf9b1bde1
shiro <shiro@usagi.io>
2020-03-30 22:10:31 +0200

    Use a hash map to look up values

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,38 @@
 extern crate jack;
 
+use jack::{Client, Control};
+use std::collections::HashMap;
 use std::io;
 use std::sync::{mpsc, Mutex};
-use jack::{Control, Client};
 
 fn main() {
+    let mut map_table = HashMap::new();
+    map_table.insert(
+        String::from("deadbeef:deadbeef_1"),
+        String::from("Non-Mixer/music:in-1"),
+    );
+    map_table.insert(
+        String::from("deadbeef:deadbeef_2"),
+        String::from("Non-Mixer/music:in-2"),
+    );
+
     let (client, _status) =
         jack::Client::new("rust-autoconnect", jack::ClientOptions::NO_START_SERVER).unwrap();
-
 
     let (tx, rx) = mpsc::channel::<Data>();
     let mtx = Mutex::new(tx);
 
-    let process = jack::ClosureProcessHandler::new(move |_: &jack::Client, _: &jack::ProcessScope| Control::Continue);
-    let notifications = Notifications::new(mtx);
+    let process =
+        jack::ClosureProcessHandler::new(move |_: &jack::Client, _: &jack::ProcessScope| {
+            Control::Continue
+        });
+    let notifications = Notifications::new(mtx, Mutex::new(map_table));
     let active_client = client.activate_async(notifications, process).unwrap();
 
-
     for data in rx {
-        active_client.as_client().connect_ports_by_name(data.from.as_str(), data.to.as_str());
+        active_client
+            .as_client()
+            .connect_ports_by_name(data.from.as_str(), data.to.as_str());
     }
 }
 
@@ -28,30 +42,32 @@ struct Data {
 }
 
 struct Notifications {
-    ch: Mutex<std::sync::mpsc::Sender<Data>>
+    ch: Mutex<std::sync::mpsc::Sender<Data>>,
+    map_table: Mutex<HashMap<String, String>>,
 }
 
 impl Notifications {
-    pub fn new(ch: Mutex<mpsc::Sender<Data>>) -> Self {
-        Notifications { ch }
+    pub fn new(ch: Mutex<mpsc::Sender<Data>>, map_table: Mutex<HashMap<String, String>>) -> Self {
+        Notifications { ch, map_table }
     }
 }
 
 impl jack::NotificationHandler for Notifications {
     fn port_registration(&mut self, client: &Client, _port_id: u32, is_registered: bool) {
-        if !is_registered { return; }
+        if !is_registered {
+            return;
+        }
 
         let strings = client.ports(None, None, jack::PortFlags::empty());
 
         for string in strings {
-            match string.as_str() {
-                "deadbeef:deadbeef_1" => {
-                    self.ch.get_mut().unwrap().send(Data { from: string, to: "Non-Mixer/music:in-1".to_string() });
+            for (key, value) in self.map_table.lock().unwrap().iter() {
+                if string == *key {
+                    self.ch.get_mut().unwrap().send(Data {
+                        from: key.clone(),
+                        to: value.clone(),
+                    });
                 }
-                "deadbeef:deadbeef_2" => {
-                    self.ch.get_mut().unwrap().send(Data { from: string, to: "Non-Mixer/music:in-2".to_string() });
-                }
-                _ => ()
             }
         }
     }
